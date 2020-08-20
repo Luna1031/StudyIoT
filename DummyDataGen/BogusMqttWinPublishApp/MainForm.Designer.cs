55ffd0c4b4583723e16198ecc998835ce1153907
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-08-20 13:56:09 +0900

    20082020

---
+++ DummyDataGen/BogusMqttWinPublishApp/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BogusMqttWinPublishApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lb = new MetroFramework.Controls.MetroLabel();
+            this.TxtBrokerIp = new MetroFramework.Controls.MetroTextBox();
+            this.BtnConnect = new MetroFramework.Controls.MetroButton();
+            this.RtbLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Lb
+            // 
+            this.Lb.AutoSize = true;
+            this.Lb.Location = new System.Drawing.Point(26, 108);
+            this.Lb.Name = "Lb";
+            this.Lb.Size = new System.Drawing.Size(103, 19);
+            this.Lb.TabIndex = 3;
+            this.Lb.Text = "MQTT Broker IP";
+            // 
+            // TxtBrokerIp
+            // 
+            // 
+            // 
+            // 
+            this.TxtBrokerIp.CustomButton.Image = null;
+            this.TxtBrokerIp.CustomButton.Location = new System.Drawing.Point(488, 1);
+            this.TxtBrokerIp.CustomButton.Name = "";
+            this.TxtBrokerIp.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtBrokerIp.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtBrokerIp.CustomButton.TabIndex = 1;
+            this.TxtBrokerIp.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtBrokerIp.CustomButton.UseSelectable = true;
+            this.TxtBrokerIp.CustomButton.Visible = false;
+            this.TxtBrokerIp.Lines = new string[] {
+        "localhost"};
+            this.TxtBrokerIp.Location = new System.Drawing.Point(131, 103);
+            this.TxtBrokerIp.MaxLength = 32767;
+            this.TxtBrokerIp.Name = "TxtBrokerIp";
+            this.TxtBrokerIp.PasswordChar = '\0';
+            this.TxtBrokerIp.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtBrokerIp.SelectedText = "";
+            this.TxtBrokerIp.SelectionLength = 0;
+            this.TxtBrokerIp.SelectionStart = 0;
+            this.TxtBrokerIp.ShortcutsEnabled = true;
+            this.TxtBrokerIp.Size = new System.Drawing.Size(510, 23);
+            this.TxtBrokerIp.TabIndex = 4;
+            this.TxtBrokerIp.Text = "localhost";
+            this.TxtBrokerIp.UseSelectable = true;
+            this.TxtBrokerIp.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtBrokerIp.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(647, 103);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(130, 23);
+            this.BtnConnect.TabIndex = 5;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseSelectable = true;
+            // 
+            // RtbLog
+            // 
+            this.RtbLog.Location = new System.Drawing.Point(26, 130);
+            this.RtbLog.Name = "RtbLog";
+            this.RtbLog.Size = new System.Drawing.Size(751, 288);
+            this.RtbLog.TabIndex = 6;
+            this.RtbLog.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RtbLog);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.TxtBrokerIp);
+            this.Controls.Add(this.Lb);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MQTT Fake Publisher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel Lb;
+        private MetroFramework.Controls.MetroTextBox TxtBrokerIp;
+        private MetroFramework.Controls.MetroButton BtnConnect;
+        private System.Windows.Forms.RichTextBox RtbLog;
+    }
+}
+
---
+++ DummyDataGen/BogusMqttWinPublishApp/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using Bogus;
+using MetroFramework.Forms;
+using Model;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace BogusMqttWinPublishApp
+{
+    public partial class MainForm : MetroForm
+    {
+        public string MqttBrokerUrl { get; set; }
+
+        public Thread MqttThread { get; set; }
+
+        public MqttClient BrokerClient { get; set; }
+
+        private Faker<SensorInfo> SensorFaker { get; set; }
+
+        private string CurrValue { get; set; }
+        public BackgroundWorker MqttWorker { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitializeAll();    // 전체 초기화
+            ConnectMqttBroker();    // MQTT 브로커 접속
+            StartPublish();     // fake 센싱 메시지 전송
+        }
+
+        private void ConnectMqttBroker()
+        {
+            MqttBrokerUrl = TxtBrokerIp.Text;
+            BrokerClient = new MqttClient(MqttBrokerUrl);
+            BrokerClient.Connect("FakerDaemon");
+        }
+
+        private void StartPublish()
+        {
+            MqttThread = new Thread(new ThreadStart(LoopPublish));
+            MqttThread.Start();
+        }
+
+        private void InitializeAll()
+        {
+            MqttWorker = new BackgroundWorker();
+            MqttWorker.DoWork += MqttWorker_DoWork;
+            MqttWorker.WorkerReportsProgress = false;
+            MqttWorker.WorkerSupportsCancellation = true;
+
+            MqttBrokerUrl = "localhost";    // 127.0.0.1 또는 210.119.12.57
+
+            string[] Rooms = new[] { "DiningRoom", "LivingRoom", "BathRoom", "BedRoom", "GuestRoom" };
+
+            SensorFaker = new Faker<SensorInfo>()
+                .RuleFor(s => s.Dev_Id, f => f.PickRandom(Rooms))
+                .RuleFor(s => s.Curr_Time, f => f.Date.Past(0).ToString("yyyy-MM-dd HH:mm:ss.ff"))
+                .RuleFor(s => s.Temp, f => float.Parse(f.Random.Float(19.0f, 35.9f).ToString("0.00")))
+                .RuleFor(s => s.Humid, f => float.Parse(f.Random.Float(40.0f, 63.9f).ToString("0.0")))
+                .RuleFor(s => s.Press, f => float.Parse(f.Random.Float(800.0f, 999.9f).ToString("0.0")));
+        }
+
+        private void MqttWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            LoopPublish();
+        }
+
+        private void LoopPublish()
+        {
+            while (true)
+            {
+                SensorInfo value = SensorFaker.Generate();
+                CurrValue = JsonConvert.SerializeObject(value, Formatting.Indented);
+                BrokerClient.Publish("home/device/data", Encoding.Default.GetBytes(CurrValue));
+                // Console.WriteLine($"Published : {CurrValue}");
+                this.Invoke(new Action(() =>
+                {
+                    RtbLog.AppendText($"\n\nPublished : {CurrValue}");
+                    RtbLog.ScrollToCaret();
+                }));
+
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
